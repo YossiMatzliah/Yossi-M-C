80da05e8cd5f57b7731c6c3387e5226d2dd7e948
yossi.matzliah <yossimatzliah@gmail.com>
2023-01-03 20:18:02 +0200

    add some string funcs and removed? .md

--- c/strings/string_funcs.c
+++ c/strings/string_funcs.c
@@ -1,5 +1,5 @@
 
-/* return the length of a pointer */
+/* StrLen: return the length of a pointer */
 size_t StrLen (const char *s)
 {
 	size_t counter = 0;
@@ -14,5 +14,121 @@ size_t StrLen (const char *s)
 	}
 	return counter;
 }
+
+/* StrCmp: function compares the two strings s1 and s2. 
+   		Returns:
+   • 0, if the s1 and s2 are equal;
+   • a negative value if s1 is less than s2;
+   • a positive value if s1 is greater than s2.
+ */
+int StrCmp(const char *s1, const char *s2)
+{
+	if (NULL == s1 || NULL == s2)
+	{
+		exit (-1);
+	}	 
+	while ('\0' != *s1 && '\0' != *s2)
+	{
+		s1++;
+		s2++;
+	}	
+	if ('\0' == *s1 && '\0' == *s2)
+	{
+		return 0;
+	}
+	else if ('\0' == *s1)
+	{
+		return -1;
+	}
+	else 
+	{
+		return 1;		
+	}
+}
+
+/* StrCpy: function coppies string source to destination. */
+
+char *StrCpy (char *dest, const char *src)
+{
+	char *dest_holder = dest; 
+	if (NULL == dest || NULL == src)
+	{
+		exit (-1);
+	}
+	while ('\0' != *src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return dest_holder;
+	
+}
+ 
+/* StrNCpy: function coppies n bytes (amount) of characters from 
+string source to destination. */
+
+char *StrNCpy(char *dest, const char *src, size_t n)
+{
+	char *dest_holder = dest;
+	size_t i;
+	if (NULL == dest || NULL == src)
+	{
+		exit (-1);
+	}
+	for (i=1; i<=n; i++)
+	{
+		if ('\0' != *src)
+		{
+			*dest = *src;
+			dest++;
+			src++;
+		}
+		else
+		{
+			*dest = *src;
+			dest++;
+		}
+	}
+	if ('\0' != *(dest-1))
+	{
+		printf ("WARNING: dest is ended without null-terminator\n");
+	}
+	return dest_holder;
+}
+
+
+/* StrNCmp: function compares the first n bytes (amount)of two strings s1 and s2. 
+   		Returns:
+   • 0, if the s1 and s2 are equal;
+   • a negative value if s1 is less than s2;
+   • a positive value if s1 is greater than s2.
+ */
+int StrNCmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+	if (NULL == s1 || NULL == s2)
+	{
+		exit (-1);
+	}
+	for (i=1; i<=n; i++)
+	{
+		s1++;
+		s2++;
+		if ('\0' == *s1 && '\0' != *s2)
+		{		
+			return -1;
+		}
+		else if ('\0' != *s1 && '\0' == *s2)
+		{		
+			return 1;
+		}
+	}
+	return 0;	
+} 
+
+
+
 
 
--- c/strings/string_main.c
+++ c/strings/string_main.c
@@ -5,13 +5,73 @@
 
 int main ()
 {
-	/* StrLen */
+				/* StrLen */
 	/*
 	const char string[] = "Damn";	
 	printf("The string length is: %li \n", StrLen(string) );	
 	*/
 	
+				/* StrCmp */
+	/*
+	char s1[] = "damn";
+	char s2[] = "right";
+	int result;
 	
+	result = StrCmp(s1, s2);
+	if (0 == result)
+	{
+		printf ("The strings lexicographically equal\n");
+	}
+	else if (result > 0)
+	{
+		printf ("The first string is lexicographically greater"
+		" than the second\n"); 
+	}	
+	else
+	{
+		printf ("The first string is lexicographically less" 
+		" than the second\n");
+	}
+	*/
+	
+				/* StrCpy */
+	/*
+	char src[] = "damn";
+	char dest[] = "righ";
+	printf ("The coppied string is: %s \n", StrCpy (dest, src));
+	*/
+	
+				/* StrNCpy */
+	/*
+	char src[] = "damn ";
+	char dest[] = "it's very hard";
+	size_t n = 6;
+	printf ("The coppied string is: %s \n", StrNCpy (dest, src, n));
+	*/
+	
+	
+				/* StrNCmp */
+	/*
+	char s1[] = "damn I'm so";
+	char s2[] = "right";
+	size_t n = 6;
+	int result;
+	result = StrNCmp(s1, s2, n);
+	if (0 == result)
+	{
+		printf ("The first n bytes of the strings lexicographically equal\n");
+	}
+	else if (result > 0)
+	{
+		printf ("The first n bytes of the first string is "
+		"lexicographically greater than the second\n"); 
+	}	
+	else
+	{
+		printf ("The first n bytes of the first string is "
+		"lexicographically less than the second\n");
+	}
+	*/
 	
 	return 1;
 }
