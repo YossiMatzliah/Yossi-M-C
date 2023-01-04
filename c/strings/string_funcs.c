385c47880b6749d1645a62fb30ddd773d694b752
yossi.matzliah <yossimatzliah@gmail.com>
2023-01-04 11:03:09 +0200

    clean and

--- c/strings/string_funcs.c
+++ c/strings/string_funcs.c
@@ -2,48 +2,40 @@
 /* StrLen: return the length of a pointer */
 size_t StrLen (const char *s)
 {
-	size_t counter = 0;
+	const char *ptr = s;
+	
 	if (NULL == s)
 	{
 		exit (-1);
 	}
+	
 	while ('\0' != *s)
 	{
 		s++;
-		counter++;
 	}
-	return counter;
+	return s - ptr;
 }
 
-/* StrCmp: function compares the two strings s1 and s2. 
-   		Returns:
+/* StrCmp: function compares two strings s1 and s2 lexicographically-wise. 
+   		Returns for each iteration:
    • 0, if the s1 and s2 are equal;
    • a negative value if s1 is less than s2;
    • a positive value if s1 is greater than s2.
- */
+*/
 int StrCmp(const char *s1, const char *s2)
 {
 	if (NULL == s1 || NULL == s2)
 	{
 		exit (-1);
 	}	 
-	while ('\0' != *s1 && '\0' != *s2)
+	
+	while ('\0' != *s1 && '\0' != *s2 && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}	
-	if ('\0' == *s1 && '\0' == *s2)
-	{
-		return 0;
-	}
-	else if ('\0' == *s1)
-	{
-		return -1;
-	}
-	else 
-	{
-		return 1;		
-	}
+	
+	return *s1 - *s2;
 }
 
 /* StrCpy: function coppies string source to destination. */
@@ -51,16 +43,19 @@ int StrCmp(const char *s1, const char *s2)
 char *StrCpy (char *dest, const char *src)
 {
 	char *dest_holder = dest; 
+	
 	if (NULL == dest || NULL == src)
 	{
 		exit (-1);
 	}
+	
 	while ('\0' != *src)
 	{
 		*dest = *src;
 		dest++;
 		src++;
 	}
+	
 	*dest = '\0';
 	return dest_holder;
 	
@@ -73,62 +68,114 @@ char *StrNCpy(char *dest, const char *src, size_t n)
 {
 	char *dest_holder = dest;
 	size_t i;
+	
 	if (NULL == dest || NULL == src)
 	{
 		exit (-1);
 	}
+	
 	for (i=1; i<=n; i++)
 	{
+		
 		if ('\0' != *src)
 		{
 			*dest = *src;
 			dest++;
 			src++;
 		}
+		
 		else
 		{
 			*dest = *src;
 			dest++;
 		}
 	}
+	
 	if ('\0' != *(dest-1))
 	{
 		printf ("WARNING: dest is ended without null-terminator\n");
 	}
+	
 	return dest_holder;
 }
 
 
-/* StrNCmp: function compares the first n bytes (amount)of two strings s1 and s2. 
-   		Returns:
+/* StrNCmp: function compares the first n bytes (amount)of 
+two strings s1 and s2 lexicographically-wise. 
+   		Returns for each iteration:
    • 0, if the s1 and s2 are equal;
    • a negative value if s1 is less than s2;
    • a positive value if s1 is greater than s2.
- */
+*/
 int StrNCmp(const char *s1, const char *s2, size_t n)
 {
 	size_t i;
+	
 	if (NULL == s1 || NULL == s2)
 	{
 		exit (-1);
 	}
+	
 	for (i=1; i<=n; i++)
 	{
-		s1++;
-		s2++;
-		if ('\0' == *s1 && '\0' != *s2)
-		{		
-			return -1;
+		
+		if (*s1 == *s2)
+		{ 
+			s1++;
+			s2++;
 		}
-		else if ('\0' != *s1 && '\0' == *s2)
-		{		
-			return 1;
-		}
-	}
-	return 0;	
+	}	
+	
+	return *s1 - *s2;
 } 
 
+/* StrCaseCmp: function compares two strings s1 and s2 lexicographically-wise
+   while upper-case are lowered to low-case
+   		Returns for each iteration:
+   • 0, if the s1 and s2 are equal;
+   • a negative value if s1 is less than s2;
+   • a positive value if s1 is greater than s2.
+*/
+int StrCaseCmp(const char *s1, const char *s2)
+{
+	int sum = 0;
+	if (NULL == s1 || NULL == s2)
+	{
+		exit (-1);
+	}
 
+	while ('\0' != *s1 && '\0' != *s2)
+	{
+		sum += (((int)(tolower(*s1)))-((int)(tolower(*s2)))); 
+		s1++;
+		s2++;
+	}	
+	printf ("The sum lexicographically-wise is: %d \n", sum);
+	return sum;
+}
+
+/* StrChr: function that search for the firs incaunter with a specific char.
+	returns:
+	• if finds: pointer to the first incaunter;
+    • if don't: NULL;
+*/
+char *StrChr (const char *s, int c)
+{
+	if (NULL == s)
+	{
+		exit (-1);
+	}
+	while (*s != (char) c) 
+	{
+		if ('\0' == *s)
+		{
+			printf ("The char wasn't found");
+			return NULL;
+		}
+		s++;
+	}
+	return (char *)s;
+}  
 
 
 
--- c/strings/string_main.c
+++ c/strings/string_main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "string_funcs.c"
 
 
@@ -13,26 +14,11 @@ int main ()
 	
 				/* StrCmp */
 	/*
-	char s1[] = "damn";
-	char s2[] = "right";
-	int result;
-	
-	result = StrCmp(s1, s2);
-	if (0 == result)
-	{
-		printf ("The strings lexicographically equal\n");
-	}
-	else if (result > 0)
-	{
-		printf ("The first string is lexicographically greater"
-		" than the second\n"); 
-	}	
-	else
-	{
-		printf ("The first string is lexicographically less" 
-		" than the second\n");
-	}
+	char s1[] = "aaa";
+	char s2[] = "abb";
+	printf ("The diff of the strings lexicographically-wise is: %d \n",StrCmp(s1, s2));
 	*/
+	
 	
 				/* StrCpy */
 	/*
@@ -49,29 +35,50 @@ int main ()
 	printf ("The coppied string is: %s \n", StrNCpy (dest, src, n));
 	*/
 	
-	
 				/* StrNCmp */
 	/*
-	char s1[] = "damn I'm so";
-	char s2[] = "right";
-	size_t n = 6;
-	int result;
-	result = StrNCmp(s1, s2, n);
-	if (0 == result)
+	char s1[] = "aaacd";
+	char s2[] = "aaafd";
+	size_t n = 5;
+	
+	 printf ("The diff of the first n bytes of the strings " 
+	 "lexicographically-wise is: %d \n",StrNCmp(s1, s2, n));
+	*/
+	
+					/* StrCaseCmp */
+	/*
+	char s1[] = "BBB";
+	char s2[] = "aaa";
+	int sum;
+	
+	sum = StrCaseCmp(s1, s2);
+	if (0 == sum)
 	{
-		printf ("The first n bytes of the strings lexicographically equal\n");
+		printf ("The strings lexicographically equal\n");
 	}
-	else if (result > 0)
+	else if (sum > 0)
 	{
-		printf ("The first n bytes of the first string is "
-		"lexicographically greater than the second\n"); 
+		printf ("The first string is lexicographically greater"
+		" than the second\n"); 
 	}	
 	else
 	{
-		printf ("The first n bytes of the first string is "
-		"lexicographically less than the second\n");
+		printf ("The first string is lexicographically less" 
+		" than the second\n");
 	}
 	*/
+	
+					/* StrChr */
+	/*
+	char s[] = "abcde";
+	char c = 'f';
+	char *ptr_to_first_find; 
+	printf ("Original string is: %s \n", s);				
+	ptr_to_first_find = StrChr(s, c);
+	printf ("The first occurrence of the '%c' in"
+	" '%s' string  is: '%s' \n", c, s, ptr_to_first_find);    
+	*/
+	
 	
 	return 1;
 }
