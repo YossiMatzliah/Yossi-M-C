126fbf9c5980c4bc4afed3eeab9d4a59b1fc8cd1
yossi.matzliah <yossimatzliah@gmail.com>
2023-01-24 12:00:13 +0200

    word bound fix

--- c/word_boundery/word_boundary.c
+++ c/word_boundery/word_boundary.c
@@ -8,12 +8,12 @@
 #define BYTE_WORD_SIZE   (__WORDSIZE/8)
 #define COPY_INT_TO_WORD(a)   (0x0101010101010101*a)
 
-static size_t CopyOneByte(char *runner, int c, size_t n);
-static size_t CopyOneByteToTwoRunners(char *runner1, char *runner2, size_t n);
+size_t CopyOneByte(char *runner, int c, size_t n);
+/*size_t CopyOneByteToTwoRunners(char *runner1, char *runner2, size_t n);*/
 
-static size_t CopyOneByte(char *runner, int c, size_t n)
+size_t CopyOneByte(char *runner, int c, size_t n)
 {
-	if (n > 0)
+	while (n > 0)
 	{
 		*runner = c;
 		++runner;
@@ -22,10 +22,10 @@ static size_t CopyOneByte(char *runner, int c, size_t n)
 	
 	return n;
 }
-
-static size_t CopyOneByteToTwoRunners(char *runner1, char *runner2, size_t n)
+/*
+size_t CopyOneByteToTwoRunners(char *runner1, char *runner2, size_t n)
 {
-	if (n > 0)
+	while (n > 0)
 	{
 		*runner1 = *runner2;
 		++runner1;
@@ -35,23 +35,21 @@ static size_t CopyOneByteToTwoRunners(char *runner1, char *runner2, size_t n)
 	
 	return n;
 }
-
+*/
 void *MemSet (void *str, int c, size_t n)
 {
 	char *runner = (char *)str;
 
 	while (n && 0 != ((size_t) runner) % BYTE_WORD_SIZE ) /* while not aligned */
 	{
-		CopyOneByte (runner, c, n);							
-		printf ("%s \n", (char *)str);						
+		CopyOneByte (runner, c, n);													
 	}
 	
 	while (BYTE_WORD_SIZE < n) 
 	{
 		*((size_t *)(runner)) = COPY_INT_TO_WORD(c);
 		runner += BYTE_WORD_SIZE;
-		n -= BYTE_WORD_SIZE;
-		printf ("%s \n", (char *)str);												
+		n -= BYTE_WORD_SIZE;												
 	}
 	n = CopyOneByte (runner, c, n);
 
@@ -60,10 +58,10 @@ void *MemSet (void *str, int c, size_t n)
 
 void *MemCpy (void *dest, const void * src, size_t n)
 {
-	char* src_char = NULL;
-	char* dest_char = NULL;
 	size_t *src_eight_bytes = (size_t *) src;
 	size_t *dest_eight_bytes = (size_t *)dest;
+	unsigned char* src_char = NULL;
+	unsigned char* dest_char = NULL;
 	/*
 	while (0 != *((unsigned int *)dest) % BYTE_WORD_SIZE &&
 	  0 != *((unsigned int *)src) % BYTE_WORD_SIZE && 0 != n)
@@ -79,48 +77,60 @@ void *MemCpy (void *dest, const void * src, size_t n)
 		n -= BYTE_WORD_SIZE;									
 	}
 	
-	dest_char = (char *)dest_eight_bytes;	
-	src_char = (char *)src_eight_bytes;
+	dest_char = (unsigned char *)dest_eight_bytes;	
+	src_char = (unsigned char *)src_eight_bytes;
 
-	n = CopyOneByteToTwoRunners(dest_char, src_char, n);
-	return dest;												
+	while (n > 0)
+	{
+		*dest_char = *src_char;
+		++dest_char;
+		++src_char;
+		--n;	
+	}
+	return dest;											
 }
 
 void *MemMove (void *dest, const void *src, size_t n)
 {
-	unsigned char *dest_runner = (unsigned char *)dest;
-	unsigned char *src_runner = (unsigned char *)src;
+	size_t diff = (size_t)dest - (size_t)src;
+	size_t i = 0;
+	unsigned char *dest_runner = (unsigned char*)((size_t *)dest + n - 1);
+	unsigned char *src_runner = (unsigned char *)((size_t) src + n - 1);
 	
-	while ( BYTE_WORD_SIZE < n )
+	if(((size_t)src < (size_t)dest) && (((size_t)src + n) > (size_t)dest))
 	{
-		*((size_t *)(dest_runner)) = *((size_t *)(src_runner));
-		dest_runner += BYTE_WORD_SIZE;
-		src_runner += BYTE_WORD_SIZE;
-		n -= BYTE_WORD_SIZE;												
+		for (i = 0; i < (n - diff); ++i)
+		{
+			*dest_runner = *src_runner;
+			--dest_runner;
+			--src_runner;
+		}
+		MemCpy(dest, src, diff);
 	}
-	
-	n = CopyOneByteToTwoRunners((char *)dest_runner, (char *)src_runner, n);													
-	
+	else
+	{
+	MemCpy(dest, src, n);
+	}
+								
 	return dest;
 }
 
 void WordBounderyTest ()
 {
-	char text1[] = "zxcvbnm1234567890";
-	char text2[] = "qwertyuiopasdfghj";
-	printf ("Before changes: %s \n",text1);
+	char text1[] = "1234567890zxcvbnmlkjhgf";
+	char text2[] = "qwertyuiopasdfghj987654";
+	printf ("Before changes:\n%s \n",text1);
 	printf ("%s \n",text2);
-	/*MemSet ((void *)text1, 'B', 9);
+	MemSet ((void *)text1, 'B', 9);
 	MemSet ((void *)text2, 'A', 10);
 	printf ("after MemSet: %s \n",text1);
-	printf ("%s \n",text2);*/
+	printf ("%s \n",text2);
 	printf ("__________ \n");
 	
-	memcpy ((void *)(text1 + 2),(const void *)(text1), 10);
-	printf ("After MemCpy: %s \n",text1);
-	/* MemCpy won't overlap cause used of functions with local pointers. should change it */
-
-	MemMove ((void *)(text1),(const void *)text2, 10);
+	memcpy ((text1),(text2), 7);
+	printf ("After MemCpy:  %s \n",text1);
+	
+	memmove ((text1),(text1 + 8), 18);
 	printf ("After MemMove: %s \n",text1);
 }
 
--- c/word_boundery/word_boundary_test.c
+++ c/word_boundery/word_boundary_test.c
@@ -4,8 +4,5 @@
 int main ()
 {
 	WordBounderyTest();
-	
-	
-	
 	return 0;
 }
